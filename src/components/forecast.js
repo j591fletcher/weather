1238d25c1b32df56cf9ad0a0569a1124daa6d345
Jamie Fletcher <j591fletcher@gmail.com>
2022-05-05 20:22:03 -0400

    initialized forecast card

---
+++ src/components/forecast.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import { Card } from 'semantic-ui-react';
+
+export default function Forecast(){
+
+    return(
+        <div>
+            <Card>
+                <Card.Content>
+                    <Card.Header>Matthew Harris</Card.Header>
+                    <Card.Meta>Co-Worker</Card.Meta>
+                    <Card.Description>
+                        Matthew is a pianist living in Nashville.
+                    </Card.Description>
+                </Card.Content>
+            </Card>
+        </div>
+
+    )
+
+}
