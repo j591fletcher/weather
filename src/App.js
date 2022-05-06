1a273219547d3cc322d62735f3b6b2762d6073d2
Jamie Fletcher <j591fletcher@gmail.com>
2022-05-05 20:05:11 -0400

    Initial weather app with basic weather information based on location

--- src/App.js
+++ src/App.js
@@ -1,24 +1,55 @@
-import logo from './logo.svg';
+import axios from 'axios';
+import { useEffect, useState } from 'react';
 import './App.css';
+import Header from './components/header';
+import WeatherCard from './components/WeatherCard'
+
+
+const URL = 'https://api.openweathermap.org/data/2.5/weather'
+const API_KEY = '2610178a7047cabec34d20c67dce4d2e'
 
 function App() {
+    
+  const [latitude, setLatitude] = useState(null);
+  const [longitude, setLongitude] = useState(null);
+  const [city, setCity] = useState('');
+  const [humidity, setHumidity] = useState(null);
+  const [temperature, setTemperature] = useState(null);
+  const [sunrise, setSunrise] = useState(null);
+  const [sunset, setSunset] = useState(null);
+  const [icon, setIcon] = useState('');
+
+  useEffect(() => {
+    navigator.geolocation.getCurrentPosition(function(position){
+      setLatitude(position.coords.latitude);
+      setLongitude(position.coords.longitude); 
+    });
+
+    axios.get(URL + '?lat=' + latitude + '&lon=' + longitude + '&appid=' + API_KEY + '&units=imperial')
+    .then((weatherData) => {
+      console.log(weatherData.data);
+      setTemperature(weatherData.data.main.temp)
+      setSunset(weatherData.data.sys.sunset)
+      setSunrise(weatherData.data.sys.sunrise)
+      setHumidity(weatherData.data.main.humidity)
+      setCity(weatherData.data.name)
+      setIcon(weatherData.data.weather[0].main)
+    })
+  }, [latitude, longitude])
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="main">
+      <Header/>
+      <WeatherCard 
+      temperature = {temperature}
+      sunset = {sunset}
+      sunrise = {sunrise}
+      humidity = {humidity}
+      city = {city}
+      icon = {icon}
+      />
     </div>
+      
   );
 }
 
---
+++ src/components/WeatherCard.js
@@ -0,0 +1,76 @@
+import React from 'react'
+import { Card, Feed } from 'semantic-ui-react'
+import moment from 'moment'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faBolt, faCloud, faCloudRain, faSmog, faSnowflake, faSun } from '@fortawesome/free-solid-svg-icons';
+
+
+const WeatherCard = (
+    { temperature, city, sunrise, sunset, humidity, icon }) => {
+        let weatherIcons = null;
+      
+        if (icon === 'Haze') {
+          weatherIcons = <FontAwesomeIcon icon={faSmog} size="lg" color="#7F8C8D" />
+        }
+        else if (icon === 'Thunderstorm') {
+          weatherIcons = <FontAwesomeIcon icon={faBolt} size="lg" color="#FAF619" />
+        }
+        else if (icon === 'Drizzle') {
+          weatherIcons = <FontAwesomeIcon icon={faCloudRain} size="lg" color="#5D6D7E" />
+        }
+        else if (icon === 'Rain') {
+          weatherIcons = <FontAwesomeIcon icon={faCloudRain} size="lg" color="#5D6D7E" />
+        }
+        else if (icon === 'Snow') {
+          weatherIcons = <FontAwesomeIcon icon={faSnowflake} size="lg" color="#D7DBDD" />
+        }
+        else if (icon === 'Mist') {
+          weatherIcons = <FontAwesomeIcon icon={faSmog} size="lg" color="#ABB2B9" />
+        }
+        else if (icon === 'Clear') {
+          weatherIcons = <FontAwesomeIcon icon={faSun} size="lg" color="#F7DC6F" />
+        }
+        else if (icon === 'Clouds') {
+          weatherIcons = <FontAwesomeIcon icon={faCloud} size="lg" color="#C9DAE9" />
+        }
+
+    return (
+        <Card>
+            <Card.Content className = "weather-card">
+            <Card.Header className = "weather-card-child">{city}</Card.Header>
+            <div className = "icon-container">
+                {weatherIcons}
+            </div>
+            </Card.Content>
+            <Card.Content>
+            <Feed>
+                <Feed.Event>
+                <Feed.Content>
+                    <h5 className = "weather-card-child">{moment().format('MMMM Do, h:mm a')}</h5>
+                    <div className = "weather-card">
+                        <div className = "weather-card-child">
+                            Temperature: {Math.floor(temperature)}°F
+                        </div>
+                        <div className = "weather-card-child">
+                            Humidity: {humidity}%
+                        </div>
+
+                    </div>
+                    <div className = "weather-card">
+                        <div className = "weather-card-child"> 
+                            Sunrise: {new Date(sunrise * 1000).toLocaleTimeString('en-US', { hour: '2-digit', minute: '2-digit'})}
+                        </div>
+                        <div className = "weather-card-child">
+                            Sunset: {new Date(sunset * 1000).toLocaleTimeString('en-US',{ hour: '2-digit', minute: '2-digit'})}
+                        </div> 
+
+                    </div>
+                </Feed.Content>
+                </Feed.Event>
+            </Feed>
+            </Card.Content>
+        </Card>
+    )
+}
+
+export default WeatherCard
